5478d0ed37783cbfacf5580b1141e2806baa7591
bisnsh <bishsh2006@outlook.com>
2025-11-01 08:28:38 +0800

    实验5-3

--- cs5/5_3/Program.cs
+++ cs5/5_3/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace _5_3
+﻿using System;
+namespace _5_3
 {
+    public class Person
+    {
+        public string name;
+        public uint age;
+        public Person(string name, uint age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("Name:{0}",name);
+            Console.WriteLine("Age:{0}",age);
+        }
+    }
+    public class Teacher:Person
+    {
+        public string teacherID;
+        public Teacher(string name, uint age, string id):base(name,age)
+        {
+            this.teacherID = id;
+        }
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine("teacherID:{0}",teacherID);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Teacher objteacher = new Teacher("Alice",30,"T12345985452");
+            objteacher.GetInfo();
         }
     }
 }
